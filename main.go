4d79bbca9873388ac4ca007f6e14f7592ce83844
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-04-05 21:48:01 +0200

    2016-04-05 minor changes before search package

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	//"log"
-	//"os"
+	"log"
+	"os"
 
-	// blank identifier, inicjalizacja z pakietu, nawet jeśli nie używamy żadnych identyfikatorów z pakietu
-	_ "matchers"
-	"search"
+	// blank identifier, umożliwia uruchomienie funkcji init z innego pliku z
+	// pakietu; taka funkcja init będzie w pliku rss.go
+
+	_ "go_book_action/go_action_sample/matchers"
+	"go_book_action/go_action_sample/search"
 )
 
+// wszystkie funkcje init w całym pakiecie zostaną wykonane przed main
+// standardowe wyjście na standardowe urządzenie wyjściowe zamiast na
+// urządzenie stderr
+func init() {
+	log.SetOutput(os.Stdout)
+}
+
 func main() {
-	//search.Run("president")
+	search.Run("president")
 }
