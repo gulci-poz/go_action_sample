dae707a358770dc9f4280609cdabdc97646bbe98
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-04-05 14:33:37 +0200

    2016-04-05 sample project layout

---
+++ exa01.go
@@ -0,0 +1,21 @@
+package main
+
+/*
+package main
+
+import "fmt"
+
+func main() {
+	// wykonanie funkcji jako goroutine
+	// kiedy i w jakim porządku powinny się wykonywać goroutines?
+	// tutaj goroutine nie zawsze się wywołuje
+	go log("hello, goroutine!")
+	fmt.Println("executing main")
+	log("hello, log!")
+	go log("hello, goroutine 2!")
+}
+
+func log(msg string) {
+	fmt.Println("executing log; user message:", msg)
+}
+*/
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	//"log"
+	//"os"
+
+	// blank identifier, inicjalizacja z pakietu, nawet jeśli nie używamy żadnych identyfikatorów z pakietu
+	_ "matchers"
+	"search"
+)
+
+func main() {
+	//search.Run("president")
+}
